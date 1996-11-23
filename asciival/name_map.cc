ee3a722a7eb4e0a1157b719b9012e85f0380e17f
James Gallagher <jgallagher@opendap.org>
1996-11-23 05:17:39 +0000

    Added name_map() because g++ wants it.

--- asciival/name_map.cc
+++ asciival/name_map.cc
@@ -5,13 +5,34 @@
 //	jhrg,jimg	James Gallagher (jgallagher@gso.uri.edu)
 
 // $Log: name_map.cc,v $
+// Revision 1.2  1996/11/23 05:17:39  jimg
+// Added name_map() because g++ wants it.
+//
 // Revision 1.1  1996/11/22 23:52:01  jimg
 // Created.
 //
 
+#ifdef __GNUG__
+#pragma implementation
+#endif
+
+#include "config_dap.h"
+
+static char rcsid[] __unused__ ={"$Id: name_map.cc,v 1.2 1996/11/23 05:17:39 jimg Exp $"};
+
+#include <Pix.h>
+#include <SLList.h>
+#include <String.h>
+
+#include "name_map.h"
+
 name_map::name_map(char *raw_equiv) 
 {
     _names.append(name_equiv(raw_equiv));
+}
+
+name_map::name_map()
+{
 }
 
 void 
