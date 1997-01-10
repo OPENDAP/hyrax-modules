99ea41845eccdc500094b3c95594b801add2a9bd
James Gallagher <jgallagher@opendap.org>
1997-01-10 06:50:32 +0000

    Added to lookup mfunc the ability to map non-alphanmerics to underscore.

--- asciival/name_map.cc
+++ asciival/name_map.cc
@@ -5,6 +5,9 @@
 //	jhrg,jimg	James Gallagher (jgallagher@gso.uri.edu)
 
 // $Log: name_map.cc,v $
+// Revision 1.3  1997/01/10 06:50:30  jimg
+// Added to lookup mfunc the ability to map non-alphanmerics to underscore.
+//
 // Revision 1.2  1996/11/23 05:17:39  jimg
 // Added name_map() because g++ wants it.
 //
@@ -18,7 +21,7 @@
 
 #include "config_dap.h"
 
-static char rcsid[] __unused__ ={"$Id: name_map.cc,v 1.2 1996/11/23 05:17:39 jimg Exp $"};
+static char rcsid[] __unused__ ={"$Id: name_map.cc,v 1.3 1997/01/10 06:50:30 jimg Exp $"};
 
 #include <Pix.h>
 #include <SLList.h>
@@ -42,13 +45,42 @@ name_map::add(char *raw_equiv)
     _names.append(equiv);
 }
 
+static String
+munge(String name)
+{
+    static Regex bad("[^A-z0-9_]", 1);
+    static Regex hex("%[0-9][0-9A-Fa-f]", 1);
+
+    while (name.contains(hex))
+	name.at(hex) = "_";
+
+    while (name.contains(bad))
+	name.at(bad) = "_";
+
+    return name;
+}
+
 String
-name_map::lookup(String name) 
+name_map::lookup(String name, const bool canonical_names = false) 
 {
     for (Pix p = _names.first(); p; _names.next(p))
-	if (name == _names(p).from)
-	    return _names(p).to;
-    return name;
+	if (name == _names(p).from) {
+	    if (!canonical_names) {
+		return _names(p).to;
+	    }
+	    else {
+		String tmp_name = _names(p).to;
+		if (tmp_name.matches(RXidentifier))
+		    return tmp_name;
+		else
+		    return munge(tmp_name);
+	    }
+	}
+
+    if (!canonical_names)
+	return name;
+    else
+	return munge(name);
 }
 
 void 
--- asciival/name_map.h
+++ asciival/name_map.h
@@ -8,6 +8,9 @@
 //	jhrg,jimg	James Gallagher (jgallagher@gso.uri.edu)
 
 // $Log: name_map.h,v $
+// Revision 1.3  1997/01/10 06:50:32  jimg
+// Added to lookup mfunc the ability to map non-alphanmerics to underscore.
+//
 // Revision 1.2  1996/11/23 05:17:56  jimg
 // Added name_map().
 //
@@ -21,6 +24,21 @@
 #ifdef __GNUG__
 #pragma interface
 #endif
+
+/// Map names using a simple thesaurus.
+/** This class can be used to build up a simple thesaurus which maps names from
+    one string to another. The thesaurus is built by calling the #add# mfunc
+    with a string of the form <source>:<dest> where <source> and <dest> are
+    string literals. Once the thesaurus is built, the #lookup# mfunc can be
+    used to get the equivalent of any string in the thesaurus. 
+
+    An additional feature, this class can canonicalize an identifier (remove
+    non-alphanumeric characters) when performing the lookup operation. This
+    is performed as a separate step from the thesaurus scan so words with no
+    entry in the thesaurus can still be canonicalized.
+
+    You can interleave calls to #add# and calls to #lookup#.
+*/
 
 class name_map {
 private:
@@ -41,13 +59,37 @@ private:
     SLList<name_equiv> _names;
 
 public:
+    /// Create a new instance of the thesaurus.
+    /** Create a new instance of the thesaurus and add the first equivalence
+        string to it.
+    */
     name_map(char *raw_equiv);
+
+    /// Create an empty thesaurus.
     name_map();
 
+    /// Add a new entry to the thesaurus.
+    /** Add a new entry to the thesaurus. The form of the new entry is
+        <source>:<dest>.
+
+	Returns: void
+    */
     void add(char *raw_equiv);
 
-    String lookup(String name);
+    /// Lookup a work in the thesaurus.
+    /** Lookup a word in the thesaurus. If the name appears in the thesaurus,
+        return its equivalent. If #canonical_names# is true, pass the string
+	#name# or its equivalent through a filter to replacing all characters
+	that are not alphanumerics with the underscore. Sequences matching
+	`%[0-9][0-9a-fA-F]' are considered to be hex escape codes and are
+	replaced by a single underscore.
 
+	Returns: the string #name#, it equivalent or its canonicalized
+	equivalent.
+    */
+    String lookup(String name, const bool canonical_names = false);
+
+    /// Delete all the entries from the thesaurus.
     void delete_all();
 };
 
