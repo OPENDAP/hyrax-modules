585928bd64456e4acb2c3ccb42963e2ecbd7ae66
James Gallagher <jgallagher@opendap.org>
2001-10-14 00:42:32 +0000

    Merged with release-3-2-8

---
+++ check_perl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Check that the perl scripts do not have debugging turned on.
+
+for script in DODS_Cache.pm DODS_Dispatch.pm FilterDirHTML.pm handler_name.pm
+do
+    if ! grep '\$debug = 0' $script >/dev/null 2>&1 \
+	|| grep -v grep '\$debug = [1-9]' $script >/dev/null 2>&1
+    then
+	exit 1;
+    fi
+done
+
+exit 0;
