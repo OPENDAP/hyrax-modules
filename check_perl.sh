c5a335d1522ee401e0532fe5ac70805139282d21
James Gallagher <jgallagher@opendap.org>
2005-05-19 14:48:47 +0000

    Update to make use of the dist target.

--- check_perl.sh
+++ check_perl.sh
@@ -2,13 +2,10 @@
 #
 # Check that the perl scripts do not have debugging turned on.
 
-for script in *.pm
-do
-    if ! grep '\$debug = 0' $script >/dev/null 2>&1 \
-	|| grep '\$debug = [1-9]' $script >/dev/null 2>&1
-    then
-	exit 1;
-    fi
-done
+files_w_debug_on="`grep -l '^my.*\$debug.*=.*[1-9].*;$' *.pm`"
+
+if test "$files_w_debug_on"; then
+    exit 1;
+fi
 
 exit 0;
